04efee17d80e073ecd8975cb3f9504548a91ef7f
곽지호 <94772949+GwakJiho@users.noreply.github.com>
2023-12-11 01:32:11 +0900

    Delete project directory

--- project/src/alret.h
+++
@@ -1,9 +0,0 @@
-// tone_generator.h
-
-#ifndef TONE_GENERATOR_H
-#define TONE_GENERATOR_H
-
-void initMyTone();
-void alret();
-
-#endif // TONE_GENERATOR_H
--- project/src/button_handler.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUTTON_HANDLER_H
-#define BUTTON_HANDLER_H
-
-#include <pthread.h>
-
-void *BtnconFunc(void *arg);
-void *BtnconFunc1(void *arg);
-
-#endif // BUTTON_HANDLER_H
